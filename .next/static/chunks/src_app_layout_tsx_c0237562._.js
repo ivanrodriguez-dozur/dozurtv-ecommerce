beba062bc3998000e7a94bf229b9259e7f375a84
dozurtv <i.d.r.bogota@gmail.com>
2025-08-14 17:08:40 -0500

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1755209320355

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__a2b81291._.css",
+  "static/chunks/node_modules_d4065aff._.js",
+  "static/chunks/src_components_ui_sonner_tsx_346cfefb._.js"
+],
+    source: "dynamic"
+});
