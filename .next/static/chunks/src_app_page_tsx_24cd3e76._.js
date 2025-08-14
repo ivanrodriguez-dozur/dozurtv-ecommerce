81a18eac6fe08e3bbe488a9d91108473dfb82031
dozurtv <i.d.r.bogota@gmail.com>
2025-08-14 17:08:38 -0500

    Add .next/static/chunks/src_app_page_tsx_24cd3e76._.js 1755209317866

---
+++ .next/static/chunks/src_app_page_tsx_24cd3e76._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_407677e9._.js",
+  "static/chunks/node_modules_cebc2c3f._.js"
+],
+    source: "dynamic"
+});
