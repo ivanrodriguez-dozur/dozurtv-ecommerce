2924d7edd545d04dee35e7f71a2b7743b2c5e6d6
dozurtv <i.d.r.bogota@gmail.com>
2025-08-14 17:04:36 -0500

    Add next.config.ts 1755209076297

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
+
+export default nextConfig
